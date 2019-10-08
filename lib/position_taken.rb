3f1f6c02bd74af22dae0e20b1ef4b89dcb0f5e49
David Strauch <dmaster18@gmail.com>
2019-10-08 21:23:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ board = []
 index = 0 
 
 def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O" 
+  if board[index] == "" || board[index] == " " ||
     false
   else
     true
